b6c85dee044d0aaf4f29445334d9d64d38fb86f4
Kankina Tetiana <tetiana.kankina@soft-loft.eu>
2022-05-08 00:28:31 +0300

    added components  NFTCard,  FocusStatusBar, SubInfo , Button

---
+++ components/index.js
@@ -0,0 +1,6 @@
+import HomeHeader from "./HomeHeader";
+import FocusStatusBar from "./FocusStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+
+export { HomeHeader, FocusStatusBar, NFTCard, CircleButton, RectButton };
