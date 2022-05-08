8fb07b8564c21e64362498f81d2ea16f86abdc1a
Kankina Tetiana <tetiana.kankina@soft-loft.eu>
2022-05-08 21:45:41 +0300

    added filter

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,23 @@
 import { useState } from "react";
-import { View, SafeArea, FlatList, Text } from "react-native";
+import { View, FlatList, Text } from "react-native";
 
 import { COLORS, NFTData } from "../constants";
 import { HomeHeader, FocusStatusBar, NFTCard } from "../components";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    filteredData.length ? setNftData(filteredData) : setNftData(NFTData);
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <FocusStatusBar background={COLORS.primary} />
@@ -13,11 +25,11 @@ const Home = () => {
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
           <FlatList
-            data={NFTData}
+            data={nftData}
             renderItem={({ item }) => <NFTCard data={item} />}
             keyExtractor={(item) => item.id}
             showsVerticalScrollIndicator={false}
-            ListHeaderComponent={<HomeHeader />}
+            ListHeaderComponent={<HomeHeader onSearch={handleSearch} />}
           />
         </View>
 
