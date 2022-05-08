bf00bb05df8cf66ae80599986a957b0a6dd0ab6a
Kankina Tetiana <tetiana.kankina@soft-loft.eu>
2022-05-08 21:44:51 +0300

    update  Details page

--- screens/Details.js
+++ screens/Details.js
@@ -1,7 +1,102 @@
-import { Text } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  FlatList,
+  Text,
+  Image,
+  StatusBar,
+} from "react-native";
 
-const Details = () => {
-  return <Text>Details</Text>;
+import { COLORS, SIZES, SHADOWS, FONTS, assets } from "../constants";
+import {
+  CircleButton,
+  RectButton,
+  FocusStatusBar,
+  SubInfo,
+  DetailsDesc,
+  DetailsBid,
+} from "../components";
+import React from "react";
+
+const DetailsHeader = ({ data, navigation }) => {
+  return (
+    <View style={{ width: "100%", height: 375 }}>
+      <Image
+        source={data.image}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%" }}
+      />
+      <CircleButton
+        imgUrl={assets.left}
+        handlePress={() => navigation.goBack()}
+        left={15}
+        top={StatusBar.currentHeight + 15}
+      />
+
+      <CircleButton
+        imgUrl={assets.heart}
+        right={15}
+        top={StatusBar.currentHeight + 15}
+      />
+    </View>
+  );
+};
+
+const Details = ({ route, navigation }) => {
+  const { data } = route.params;
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: " rgba (255, 225, 225, 0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
+
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+            <DetailsHeader data={data} navigation={navigation} />
+            <SubInfo />
+            <View style={{ padding: SIZES.font }}>
+              <DetailsDesc data={data} />
+
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontFamily: FONTS.regular,
+                    fontSize: SIZES.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bids
+                </Text>
+              )}
+            </View>
+          </React.Fragment>
+        )}
+      />
+    </SafeAreaView>
+  );
 };
 
 export default Details;
